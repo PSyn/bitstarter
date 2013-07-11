d1f8516d7caa100718aec4eb660847da52c31c7c
PK <zxb301@gmail.com>
2013-07-11 22:06:51 +0000

    Added index.html mod2

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('fs.readFile(index.html,buf.toString(String, 0, buffer.length)');
+    var fs=require('fs');
+    var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8')
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
